feb913cbcd35d88cf3578cfa6d8540cc103e2714
Rahul Roy <70642284+Rahul171201@users.noreply.github.com>
2021-08-28 10:19:31 +0530

    repo initialized

---
+++ index.js
@@ -0,0 +1,30 @@
+// requiring modules
+const express = require('express');
+const app = express();
+const helmet = require('helmet');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// admin:
+// username : Rahul | password : 12345
+
+// establishing connections
+dotenv.config();
+mongoose 
+ .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+     })    
+ .then(() => console.log("Database connected!"))
+ .catch(err => console.log(err));
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+
+app.listen('4000', () => {
+    console.log("Listening at port 4000");
+});
